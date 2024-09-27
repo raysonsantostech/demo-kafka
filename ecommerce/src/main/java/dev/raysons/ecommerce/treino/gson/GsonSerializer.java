40a189a974626844785731ca53cb49e212464ddd
Rayson Santos <103448947+raysonsantostech@users.noreply.github.com>
2024-09-27 10:21:22 +0000

    aula: corrige uso de generics no serializer e usa GsonBuilder

--- ecommerce/src/main/java/dev/raysons/ecommerce/treino/gson/GsonSerializer.java
+++ ecommerce/src/main/java/dev/raysons/ecommerce/treino/gson/GsonSerializer.java
@@ -3,12 +3,15 @@ package dev.raysons.ecommerce.treino.gson;
 import org.apache.kafka.common.serialization.Serializer;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class GsonSerializer<T> implements Serializer<T> {
 
+    private final Gson gson = new GsonBuilder().create();
+
     @Override
-    public byte[] serialize(String topic, Object data) {
-        return new Gson().toJson(data).getBytes();
+    public byte[] serialize(String topic, T data) {
+        return gson.toJson(data).getBytes();
     }
 
 }
